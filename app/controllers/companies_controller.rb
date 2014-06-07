c16bad80b65d861daa135f13ea52808355c0d7b9
Tam T Dang <teedang19@gmail.com>
2014-06-07 13:27:25 -0500

    users#show shows all users pcs

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,24 @@
 class CompaniesController < ApplicationController
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to @company # this will probably only be in the context of creating a new company_user, so we'll need to redirect elsewhere
+    else # don't have any validations right now ..
+      render :new
+    end
+  end
+
+  def show
+    @company = Company.find(params[:id])
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :address, :website, :hiring, :cto_name, :cto_email, :cto_twitter, :cto_blog_url)
+  end
 end
