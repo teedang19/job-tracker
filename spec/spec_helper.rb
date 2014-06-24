62d804447e0f2124f8f9efb6fc2903bbce5c513a
Tam T Dang <teedang19@gmail.com>
2014-06-24 09:19:45 -0500

    coveralls gem and required in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,8 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'database_cleaner'
-
+require 'coveralls'
+Coveralls.wear!
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
