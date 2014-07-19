6245e935fbce98d1941c6b86335692ae5d68b913
Tam T Dang <teedang19@gmail.com>
2014-07-19 13:00:19 -0500

    add lat & long fields to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140719175414) do
+ActiveRecord::Schema.define(version: 20140719175924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 20140719175414) do
     t.string   "city"
     t.string   "state",           limit: 2
     t.string   "zip",             limit: 5
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
