21b3e248fba828d64c9aa8fd7f07f4a9eb2090d0
Tam T Dang <teedang19@gmail.com>
2014-07-19 13:12:00 -0500

    full address method on addressable models - company, user

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,4 +3,9 @@ class Company < ActiveRecord::Base
   has_many :job_seekers, through: :company_users, source: :user
 
   validates :name, :website, presence: true, uniqueness: true
+
+  def full_address
+    street_two == "" ? "#{street} #{city}, #{state} #{zip}" : "#{street}, #{street_two} #{city}, #{state} #{zip}"
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ActiveRecord::Base
   has_many :companies, through: :potential_companies
 
   validates :username, presence: true, uniqueness: true
+
+  def full_address
+    street_two == "" ? "#{street} #{city}, #{state} #{zip}" : "#{street}, #{street_two} #{city}, #{state} #{zip}"
+  end
+
 end
