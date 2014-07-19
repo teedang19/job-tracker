5200b6a58e245c08b43902c4906d192511a30944
Tam T Dang <teedang19@gmail.com>
2014-07-19 13:40:58 -0500

    add email field to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140719180431) do
+ActiveRecord::Schema.define(version: 20140719182005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20140719180431) do
     t.string   "zip",             limit: 5
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "email"
   end
 
 end
