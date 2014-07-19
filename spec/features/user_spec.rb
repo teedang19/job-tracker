e5f524bca604e1d25680144ea8a04fb85ab27ca6
Tam T Dang <teedang19@gmail.com>
2014-07-19 14:08:50 -0500

    update tests to pass with new db fields - email, address

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature 'returning users' do
 
   before :each do
-    User.create(username: "user1", password: "password")
+    User.create(username: "user1", password: "password", password_confirmation: "password", street: "351 W. Hubbard St", city: "Chicago", state: "IL", zip: "60654", email:"tam@devbootcamp.com")
   end
 
   it 'can login with a name and password' do
@@ -20,7 +20,7 @@ end
 feature 'logged in users' do
 
   before :each do
-    @test_user = User.create(username: "user1", password: "password")
+    @test_user = User.create(username: "user1", password: "password", password_confirmation: "password", street: "351 W. Hubbard St", city: "Chicago", state: "IL", zip: "60654", email: "tam@devbootcamp.com")
 
     visit root_path
     fill_in 'session_username', with: 'user1'
@@ -42,6 +42,20 @@ feature 'logged in users' do
     visit user_path(@test_user)
 
     expect(page).to have_content("Jellyvision")
+  end
+
+  it 'can create a potential job' do
+    jellyvision = Company.create(name: "Jellyvision", website: "www.jellyvision.com")
+    visit user_path(@test_user)
+    click_link 'Add a potential job'
+    click_link 'add a potential job for Jellyvision' # the JS that makes this form submit correctly isn't available to rspec
+
+    expect {
+      click_button 'Save'
+    }.to change(CompanyUser, :count).by(1)
+  end
+
+  it "can delete a potential job" do
   end
 
 end
